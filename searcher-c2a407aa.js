ab838588a1a0c21a8f7862f5ccddce17241ee74f
sonneko <sonneko@users.noreply.github.com>
2026-03-18 12:20:42 +0000

    deploy: 1db9b066c403d8dde46389242941e4651a2ada6e

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9556dba6.js',
+                path_to_root + 'searchindex-d1018542.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
