282846aecf78c402c01d2960d8558b467cf1c60e
sonneko <sonneko@users.noreply.github.com>
2026-05-31 00:41:27 +0000

    deploy: 45f7d0d5bab020cde39b1f3050fd01beba336d64

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7c7f878d.js',
+                path_to_root + 'searchindex-4d36fb6e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
